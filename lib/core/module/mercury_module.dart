b6a945170475df07398fb1f16617a467cee065f3
Yunus AK <yunus.ak.gen.tr@gmail.com>
2021-08-03 23:42:29 +0300

    Mercury module optional height

--- lib/core/module/mercury_module.dart
+++ lib/core/module/mercury_module.dart
@@ -5,16 +5,20 @@ import 'package:mercuryos_ui_elements/core/locus_bar/locus_bar.dart';
 class MercuryModule extends StatelessWidget {
   final locusBar;
   final child;
+  final double height;
   const MercuryModule({
     Key? key,
     this.locusBar,
     this.child,
+    this.height: 300,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: const EdgeInsets.all(10),
       width: MediaQuery.of(context).size.width * 0.4,
+      height: height,
       clipBehavior: Clip.antiAlias,
       decoration: BoxDecoration(
           color: DefaultColorTheme.moduleBackgroundColor,
