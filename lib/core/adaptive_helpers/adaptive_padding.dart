6d396ad5018243e8783fa8cd22305ce92d59f567
Yunus Ak <yunus.ak.gen.tr@gmail.com>
2021-08-04 10:38:15 +0300

    LocusBar added moduleIcon

---
+++ lib/core/adaptive_helpers/adaptive_padding.dart
@@ -0,0 +1,7 @@
+class AdaptiveInsets {
+  static const double xsmall = 4;
+  static const double small = 8;
+  static const double medium = 10;
+  static const double large = 16;
+  static const double xlarge = 22;
+}
--- lib/core/locus_bar/locus_bar.dart
+++ lib/core/locus_bar/locus_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:mercuryos_ui_elements/core/adaptive_helpers/adaptive_padding.dart';
 import 'package:mercuryos_ui_elements/core/color_constants.dart';
 
 class LocusBar extends StatelessWidget {
@@ -8,25 +9,31 @@ class LocusBar extends StatelessWidget {
   final Color iconColor;
   final String placeHolder;
   final Color textColor;
+  final Icon moduleIcon;
 
   const LocusBar({
     Key? key,
     this.backgroundColor: DefaultColorTheme.locusBackgroundColor,
     this.height: 50,
     this.iconColor: DefaultColorTheme.locusSecondaryTextColor,
-    this.placeHolder: 'What would you like to do?',
+    this.moduleIcon: const Icon(Icons.grid_view_rounded),
+    this.placeHolder: 'What would you like to do? ',
     this.textColor: DefaultColorTheme.locusTextColor,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.fromLTRB(20, 10, 10, 10),
+      padding: const EdgeInsets.fromLTRB(AdaptiveInsets.large,
+          AdaptiveInsets.medium, AdaptiveInsets.medium, AdaptiveInsets.medium),
       height: height,
       color: backgroundColor,
       child: Row(
         children: [
-          Text(placeHolder, style: TextStyle(color: textColor)),
+          moduleIcon,
+          SizedBox(width: AdaptiveInsets.small),
+          Expanded(
+              child: Text(placeHolder, style: TextStyle(color: textColor))),
           Icon(Icons.mic, color: iconColor),
         ],
       ),
