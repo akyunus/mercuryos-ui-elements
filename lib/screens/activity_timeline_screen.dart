79e65e57fd3aec1629ddfcf772124d8ad5bb93b1
Yunus Ak <yunus.ak.gen.tr@gmail.com>
2021-08-04 10:38:27 +0300

    TimeLineBar added

--- lib/screens/activity_timeline_screen.dart
+++ lib/screens/activity_timeline_screen.dart
@@ -1,8 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:mercuryos_ui_elements/core/adaptive_helpers/adaptive_padding.dart';
 import 'package:mercuryos_ui_elements/core/color_constants.dart';
 import 'package:mercuryos_ui_elements/core/kiri/kiri_screen.dart';
 import 'package:mercuryos_ui_elements/core/locus_bar/locus_bar.dart';
 import 'package:mercuryos_ui_elements/core/space/space_timeline_card.dart';
+
+List<ActivityDTO> activities = [
+  ActivityDTO(activities: todaySpaces, day: 'Today'),
+  ActivityDTO(activities: yesterdaySpaces, day: 'Yesterday'),
+  ActivityDTO(activities: todaySpaces, day: '13 May 2021'),
+  ActivityDTO(activities: todaySpaces, day: '12 May 2021'),
+  ActivityDTO(activities: moreSpaces, day: 'Last Month'),
+];
+
+List<SpaceCardDTO> moreSpaces = [
+  SpaceCardDTO(
+    lastUpdated: 'Yesterday 20:25',
+    moduleIcons: [Icon(Icons.movie)],
+    spaceName: 'Watch Veep',
+    subInfo: 'Video feed from HBO',
+    thumbnailImage: 'assets/ui-designs/module_map.png',
+  ),
+  SpaceCardDTO(
+    lastUpdated: 'Updated just now',
+    spaceName: 'Review Inbox',
+    subInfo: '7 items from Mail, Messenger and Twitter',
+    moduleIcons: [
+      Icon(Icons.mail_outline),
+      Icon(Icons.messenger_rounded),
+      Icon(Icons.flaky_sharp),
+    ],
+    thumbnailImage: 'assets/ui-designs/module_map.png',
+  ),
+  SpaceCardDTO(
+    lastUpdated: 'Yesterday 20:25',
+    moduleIcons: [Icon(Icons.movie)],
+    spaceName: 'Watch Veep',
+    subInfo: 'Video feed from HBO',
+    thumbnailImage: 'assets/ui-designs/module_map.png',
+  ),
+  SpaceCardDTO(
+    lastUpdated: 'Yesterday 20:25',
+    moduleIcons: [Icon(Icons.movie)],
+    spaceName: 'Watch Veep',
+    subInfo: 'Video feed from HBO',
+    thumbnailImage: 'assets/ui-designs/module_map.png',
+  ),
+];
+
+List<SpaceCardDTO> todaySpaces = [
+  SpaceCardDTO(
+    lastUpdated: 'Updated just now',
+    spaceName: 'Review Inbox',
+    subInfo: '7 items from Mail, Messenger and Twitter',
+    moduleIcons: [
+      Icon(Icons.mail_outline),
+      Icon(Icons.messenger_rounded),
+      Icon(Icons.flaky_sharp),
+    ],
+    thumbnailImage: 'assets/ui-designs/module_map.png',
+  )
+];
+
+List<SpaceCardDTO> yesterdaySpaces = [
+  SpaceCardDTO(
+    lastUpdated: 'Yesterday 20:25',
+    moduleIcons: [Icon(Icons.movie)],
+    spaceName: 'Watch Veep',
+    subInfo: 'Video feed from HBO',
+    thumbnailImage: 'assets/ui-designs/module_map.png',
+  ),
+  SpaceCardDTO(
+    lastUpdated: 'Yesterday 20:25',
+    moduleIcons: [Icon(Icons.movie)],
+    spaceName: 'Watch Veep',
+    subInfo: 'Video feed from HBO',
+    thumbnailImage: 'assets/ui-designs/module_map.png',
+  ),
+];
+
+class ActivityDTO {
+  final String day;
+  final List<SpaceCardDTO> activities;
+  const ActivityDTO({
+    required this.activities,
+    required this.day,
+  });
+}
 
 class ActivityTimelineScreen extends StatelessWidget {
   @override
@@ -10,7 +94,7 @@ class ActivityTimelineScreen extends StatelessWidget {
     return KiriScreen(
       child: Column(
         children: [
-          LocusBar(
+          TimelineBar(
             backgroundColor: DefaultColorTheme.locusBackgroundColordark,
           ),
           Expanded(
@@ -22,10 +106,6 @@ class ActivityTimelineScreen extends StatelessWidget {
                     return _activitiesRowBuilder(context, activities[i]);
                   }),
             ),
-          ),
-          TextButton(
-            child: Text('Back < '),
-            onPressed: () => Navigator.pop(context),
           ),
         ],
       ),
@@ -93,85 +173,41 @@ class SpaceCardDTO {
   });
 }
 
-class ActivityDTO {
-  final String day;
-  final List<SpaceCardDTO> activities;
-  const ActivityDTO({
-    required this.activities,
-    required this.day,
-  });
+class TimelineBar extends StatelessWidget {
+  final Color backgroundColor;
+  final double height;
+  final Color iconColor;
+  final Color textColor;
+  final String placeHolderText;
+
+  const TimelineBar(
+      {Key? key,
+      this.backgroundColor: DefaultColorTheme.locusBackgroundColor,
+      this.height: 50,
+      this.iconColor: DefaultColorTheme.locusSecondaryTextColor,
+      this.textColor: DefaultColorTheme.locusTextColor,
+      this.placeHolderText: 'What would you like to do?'})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(
+          AdaptiveInsets.large, 0, AdaptiveInsets.medium, 0),
+      height: height,
+      color: backgroundColor,
+      child: Row(
+        children: [
+          Expanded(
+              child: Row(
+            children: [
+              Text(placeHolderText, style: TextStyle(color: textColor)),
+              Icon(Icons.mic, color: iconColor),
+            ],
+          )),
+          Text('09:41', style: TextStyle(color: textColor)),
+        ],
+      ),
+    );
+  }
 }
-
-List<SpaceCardDTO> yesterdaySpaces = [
-  SpaceCardDTO(
-    lastUpdated: 'Yesterday 20:25',
-    moduleIcons: [Icon(Icons.movie)],
-    spaceName: 'Watch Veep',
-    subInfo: 'Video feed from HBO',
-    thumbnailImage: 'assets/ui-designs/module_map.png',
-  ),
-  SpaceCardDTO(
-    lastUpdated: 'Yesterday 20:25',
-    moduleIcons: [Icon(Icons.movie)],
-    spaceName: 'Watch Veep',
-    subInfo: 'Video feed from HBO',
-    thumbnailImage: 'assets/ui-designs/module_map.png',
-  ),
-];
-
-List<SpaceCardDTO> todaySpaces = [
-  SpaceCardDTO(
-    lastUpdated: 'Updated just now',
-    spaceName: 'Review Inbox',
-    subInfo: '7 items from Mail, Messenger and Twitter',
-    moduleIcons: [
-      Icon(Icons.mail_outline),
-      Icon(Icons.messenger_rounded),
-      Icon(Icons.flaky_sharp),
-    ],
-    thumbnailImage: 'assets/ui-designs/module_map.png',
-  )
-];
-
-List<SpaceCardDTO> moreSpaces = [
-  SpaceCardDTO(
-    lastUpdated: 'Yesterday 20:25',
-    moduleIcons: [Icon(Icons.movie)],
-    spaceName: 'Watch Veep',
-    subInfo: 'Video feed from HBO',
-    thumbnailImage: 'assets/ui-designs/module_map.png',
-  ),
-  SpaceCardDTO(
-    lastUpdated: 'Updated just now',
-    spaceName: 'Review Inbox',
-    subInfo: '7 items from Mail, Messenger and Twitter',
-    moduleIcons: [
-      Icon(Icons.mail_outline),
-      Icon(Icons.messenger_rounded),
-      Icon(Icons.flaky_sharp),
-    ],
-    thumbnailImage: 'assets/ui-designs/module_map.png',
-  ),
-  SpaceCardDTO(
-    lastUpdated: 'Yesterday 20:25',
-    moduleIcons: [Icon(Icons.movie)],
-    spaceName: 'Watch Veep',
-    subInfo: 'Video feed from HBO',
-    thumbnailImage: 'assets/ui-designs/module_map.png',
-  ),
-  SpaceCardDTO(
-    lastUpdated: 'Yesterday 20:25',
-    moduleIcons: [Icon(Icons.movie)],
-    spaceName: 'Watch Veep',
-    subInfo: 'Video feed from HBO',
-    thumbnailImage: 'assets/ui-designs/module_map.png',
-  ),
-];
-
-List<ActivityDTO> activities = [
-  ActivityDTO(activities: todaySpaces, day: 'Today'),
-  ActivityDTO(activities: yesterdaySpaces, day: 'Yesterday'),
-  ActivityDTO(activities: todaySpaces, day: '13 May 2021'),
-  ActivityDTO(activities: todaySpaces, day: '12 May 2021'),
-  ActivityDTO(activities: moreSpaces, day: 'Last Month'),
-];
